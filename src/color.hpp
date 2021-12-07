cd0427051c3cb6f96ddb35e7a0432032e515ba98
xuchen <xucheng@gaoding.com>
2021-12-07 08:02:37 +0800

    init

---
+++ src/color.hpp
@@ -0,0 +1,30 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+#include "common.hpp"
+#include "vec3.hpp"
+#include <iostream>
+
+void write_color(std::ostream &out, color pixel_color) {
+    out << static_cast<int>(255.999 * pixel_color.x()) << ' '
+        << static_cast<int>(255.999 * pixel_color.y()) << ' '
+        << static_cast<int>(255.999 * pixel_color.z()) << '\n';
+}
+
+void write_color(std::ostream &out, color pixel_color, int sample_per_pixel) {
+    auto r = pixel_color.x();
+    auto g = pixel_color.y();
+    auto b = pixel_color.z();
+
+    // Divide the color by the number of samples
+    auto scale = 1.0 / sample_per_pixel;
+    r = sqrt(scale * r);
+    g = sqrt(scale * g);
+    b = sqrt(scale * b);
+
+    out << static_cast<int>(255.999 * clamp(r, 0.0, 0.999)) << ' '
+        << static_cast<int>(255.999 * clamp(g, 0.0, 0.999)) << ' '
+        << static_cast<int>(255.999 * clamp(b, 0.0, 0.999)) << '\n';
+}
+
+#endif
